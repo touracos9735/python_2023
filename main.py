0f3551fe4867f21f24b321b5a199eb8fa8cbeea7
touracos9735 <27302454-touracos9735@users.noreply.replit.com>
2023-10-24 03:25:44 +0000

    Add in my comment on the script.
    A programme to explain between local and global

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 enemies = 1
 
-def increase_enemies():
+"""Between local and global variable"""
+
+def increase_enemies():        #local
   enemies = 2
   print(f"enemies inside function: {enemies}")
 
-increase_enemies()
+increase_enemies()              #global
 print(f"enemies outside function: {enemies}")
